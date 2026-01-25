08cd2543aa88847ceca3b34ff952fab2afe1a8dd
wifi-left <ysy1679682327@outlook.com>
2026-01-26 01:41:44 +0800

    修复捡东西的bug

--- src/main/java/dev/doctor4t/trainmurdermystery/mixin/ItemEntityMixin.java
+++ src/main/java/dev/doctor4t/trainmurdermystery/mixin/ItemEntityMixin.java
@@ -28,7 +28,8 @@ public abstract class ItemEntityMixin {
     @WrapMethod(method = "playerTouch")
     public void tmm$preventGunPickup(Player player, Operation<Void> original) {
         if (player.isCreative() || !this.getItem().is(TMMItemTags.GUNS)
-                || (GameWorldComponent.KEY.get(player.level()).canPickUpRevolver(player) && !player.equals(this.getOwner())
+                || (GameWorldComponent.KEY.get(player.level()).canPickUpRevolver(player)
+                        && !player.equals(this.getOwner())
                         && !player.getInventory().contains(itemStack -> itemStack.is(TMMItemTags.GUNS)))) {
             // 在拾取物品之前调用角色的onPickupItem方法
             if (dev.doctor4t.trainmurdermystery.api.RoleMethodDispatcher.callOnPickupItem(player,
